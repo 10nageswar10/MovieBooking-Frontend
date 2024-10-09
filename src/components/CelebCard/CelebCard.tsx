0596f622f876004a97a71375e77b5544fb91a411
Nageswar <iknageswar2003@gmail.com>
2024-10-09 21:37:38 +0530

    deploy third debug

--- src/components/CelebCard/CelebCard.tsx
+++ src/components/CelebCard/CelebCard.tsx
@@ -8,7 +8,7 @@ const CelebCard = (data:CelebrityCardType) => {
   console.log(data);
   return (
     <div className='celebcard'>
-    <Image src={data.celebImage} alt={data.name} width={200} height={200} />
+    <Image src={data.celebImage} alt={data.celebName} width={200} height={200} />
         <h3>{data.celebName}</h3>
         <h4>{data.celebRole}</h4>
     </div>
